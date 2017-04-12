e7fb7c87f8a2d2d1a88cba6bd0eb23a46d6e633c
Bess Sadler <bess@curationexperts.com>
2017-04-12 13:01:15 -0400

    Feature spec for show public ETD, without new fields

---
+++ spec/features/show_etd_spec.rb
@@ -0,0 +1,37 @@
+# Generated via
+#  `rails generate hyrax:work Etd`
+require 'rails_helper'
+
+RSpec.feature 'Display an ETD' do
+  let(:title) { ['China and its Minority Population'] }
+  let(:creator) { ['Eun, Dongwon'] }
+  let(:keyword) { ['China', 'Minority Population'] }
+  let(:department) { ['Department of Russian and East Asian Languages and Cultures'] }
+  let(:school) { ['Emory College of Arts and Sciences'] }
+  let(:degree) { ['Bachelor of Arts with Honors'] }
+  let(:visibility) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+  let :etd do
+    Etd.new(title: title, creator: creator, keyword: keyword, department: department, school: school, degree: degree, visibility: visibility)
+  end
+
+  context 'a logged in user' do
+    let(:user_attributes) do
+      { email: 'test@example.com' }
+    end
+    let(:user) do
+      User.new(user_attributes) { |u| u.save(validate: false) }
+    end
+
+    before do
+      login_as user
+      etd.save
+    end
+
+    scenario "Show an ETD" do
+      visit("/concern/etds/#{etd.id}")
+      expect(page).to have_content etd.title.first
+      expect(page).to have_content etd.creator.first
+      expect(page).to have_content etd.keyword.first
+    end
+  end
+end
